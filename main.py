17c1732f74e2c56937cf4fbe0d70682947da1035
EchoStormAlpha <hunterl2468@gmail.com>
2024-04-27 22:47:15 -0500

    added report function

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ def main():
     text=get_book_text(book_path)
     number_of_words=get_word_count(text)
     number_of_characters=get_number_of_each_character(text)
-    print(text)
-    print(number_of_words)
-    print(number_of_characters)
+    report(number_of_words,number_of_characters,book_path)
+   
+    
     
 def get_book_text(path):
     with open(path) as f:
@@ -24,4 +24,20 @@ def get_number_of_each_character(text):
             else:
                 num_characters[characters]+=1
         return num_characters
+
+def report(number_words,number_char,book_path):
+        
+        print("--Begin report of "+ book_path+ " --")
+        print(str(number_words)+" words found in the document")
+        letters=[]
+        
+        for value in number_char:
+            if (value.isalpha()):
+                letters.append({"name":value, "num":number_char[value]})
+        def sort_on(letters):
+             return letters['num']
+        letters.sort(reverse=True, key=sort_on)
+        for letter in letters:
+             print("The "+str(letter['name'])+" character was found "+str(letter['num'])+" times")
+        print('--End report--')
 main()
